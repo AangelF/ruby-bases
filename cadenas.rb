ebc31a9875bc5db73dd0fdb72938b5c64d8ff1df
AangelF <aangel.flores.r@gmail.com>
2024-02-06 15:48:26 -0600

    ADD: cadenas.rb and obtener_datos.rb files

---
+++ cadenas.rb
@@ -0,0 +1,7 @@
+# Cadenas de texto
+nombre = "Angel"
+
+# print por el contrario de puts, muestra el texto en consola sin dar salto de linea al final.
+# Para interpolar texto con variables se usan " comillas dobles " y #{} con la variable dentro.
+print "Hola #{nombre}"
+
---
+++ obtener_datos.rb
@@ -0,0 +1,6 @@
+# La palabra clave gets es para obtener informacion del usuario mediante la consola.
+print "Introduce tu nombre: "
+nombre = gets 
+nombre = nombre.chomp # La funcion chomp quita el salto de linea 
+print "Hola #{nombre}"
+
